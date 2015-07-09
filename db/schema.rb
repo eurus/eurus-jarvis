ecc1a50e4a8a4cad8d1baf54c2f3d51dd3072507
banana <ufozhengli@163.com>
2015-07-09 12:55:53 +0800

    add role to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709032338) do
+ActiveRecord::Schema.define(version: 20150709045053) do
 
   create_table "errands", force: :cascade do |t|
     t.string   "location",   limit: 255
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150709032338) do
     t.string   "nickname",               limit: 255
     t.datetime "join_at"
     t.datetime "leave_at"
+    t.string   "role",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

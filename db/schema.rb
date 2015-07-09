51e0203d997a45a145447b078509dd1f59fbfe4c
banana <ufozhengli@163.com>
2015-07-09 11:11:26 +0800

    add username nickname join_at leave_at to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709015439) do
+ActiveRecord::Schema.define(version: 20150709030827) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 20150709015439) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "username",               limit: 255
+    t.string   "nickname",               limit: 255
+    t.datetime "join_at"
+    t.datetime "leave_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

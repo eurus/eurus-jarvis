0dd74ac53f36a2d2003b140236c41a0901ad9011
banana <ufozhengli@163.com>
2015-07-09 13:04:13 +0800

    add articla scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articals
   resources :projects
   resources :vacations
   resources :errands
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709045721) do
+ActiveRecord::Schema.define(version: 20150709050324) do
+
+  create_table "articals", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.text     "content",    limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.string   "origin",     limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "errands", force: :cascade do |t|
     t.string   "location",   limit: 255

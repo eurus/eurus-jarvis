6945569d0f7cfa1474ddf9546d51c3d4026d6548
banana <ufozhengli@163.com>
2015-07-20 17:04:30 +0800

    use username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
   USERROLE = %w{ ceo director pm stuff intern}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -43,6 +44,23 @@ class User < ActiveRecord::Base
       return node
     else
       (User.buddies node).map { |u| User.dfs u }
+    end
+  end
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_h).first
     end
   end
 end

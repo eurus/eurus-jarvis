170345253e99be5692b10dd1def46cf368614fd9
banana <ufozhengli@163.com>
2015-07-21 16:39:57 +0800

    fix nian jia

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,17 +1,22 @@
 module DashboardHelper
   def sabbatical_remain
-    if Time.current < current_user.join_at + 1.year
-      0
-    elsif Time.current >= (current_user.join_at + 1.year)
-      if Time.current.year == (current_user.join_at + 1.year).year
-        ((Time.current.end_of_year - current_user.join_at - 1.year)/1.day).round*6/365
-      elsif  Time.current < (current_user.join_at + 10.year)
-        6
-      elsif Time.current >= (current_user.join_at + 10.year) && Time.current < (current_user.join_at + 20.year)
-        10
-      else
-        15
+    if current_user.join_at
+      if Time.current < current_user.join_at + 1.year
+        0
+      elsif Time.current >= (current_user.join_at + 1.year)
+        if Time.current.year == (current_user.join_at + 1.year).year
+          ((Time.current.end_of_year - current_user.join_at - 1.year)/1.day).round*6/365
+        elsif  Time.current < (current_user.join_at + 10.year)
+          6
+        elsif Time.current >= (current_user.join_at + 10.year) && Time.current < (current_user.join_at + 20.year)
+          10
+        else
+          15
+        end
       end
+      
+    else
+      0
     end
 
   end

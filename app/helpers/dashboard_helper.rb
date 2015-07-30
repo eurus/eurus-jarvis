2b6475c6e82c30e6d5a31113c1cb6142b3b6970b
banana <ufozhengli@163.com>
2015-07-30 14:15:55 +0800

    fix error

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,14 +1,14 @@
 module DashboardHelper
   def sabbatical_remain
     if current_user.join_at
-      if Time.current < current_user.join_at + 1.year
-        0
-      elsif Time.current >= (current_user.join_at + 1.year)
-        if Time.current.year == (current_user.join_at + 1.year).year
-          ((Time.current.end_of_year - current_user.join_at - 1.year)/1.day).round*6/365
-        elsif  Time.current < (current_user.join_at + 10.year)
+      if Date.current < current_user.join_at + 1.year
+      0
+      elsif Date.current >= (current_user.join_at + 1.year)
+        if Date.current.year == (current_user.join_at + 1.year).year
+          ((Date.current.end_of_year - current_user.join_at - 1.year)/1.day).round*6/365
+        elsif  Date.current < (current_user.join_at + 10.year)
           6
-        elsif Time.current >= (current_user.join_at + 10.year) && Time.current < (current_user.join_at + 20.year)
+        elsif Date.current >= (current_user.join_at + 10.year) && Date.current < (current_user.join_at + 20.year)
           10
         else
           15
@@ -23,8 +23,8 @@ module DashboardHelper
 
   def sabbatical_gone
     days = 0
-    current_user.vacations.where("approve = true and cut = '年假' and created_at <= ?", Time.current.end_of_year).map{|v|
-        ((v.to-v.start)/1.day).to_f
+    current_user.vacations.where("approve = true and cut = '年假' and created_at <= ?", Date.current.end_of_year).map{|v|
+        v.duration
     }.reduce(:+) || 0
   end
 

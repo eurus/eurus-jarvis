56423118f129e16705508b6edd8246db74767a74
banana <ufozhengli@163.com>
2015-07-31 14:22:00 +0800

    seed for errand

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,32 +16,32 @@ def fpo(email)
   User.find_by_email(email).id rescue 1
 end
 ### run this first to import project records
-CSV.foreach('dump_data/project.csv', headers: true, col_sep: ',') do |row|
-  Project.create(
-    name: row["name"],
-    content: row["content"],
-    created_at: row["created_at"],
-    updated_at: row["updated_at"],
-    owner: (fpo row["owner"]),
-    status: 'done')
-end
+# CSV.foreach('dump_data/project.csv', headers: true, col_sep: ',') do |row|
+#   Project.create(
+#     name: row["name"],
+#     content: row["content"],
+#     created_at: row["created_at"],
+#     updated_at: row["updated_at"],
+#     owner: (fpo row["owner"]),
+#     status: 'done')
+# end
 
 ### run this next to import vacation records
 # Vacation.delete_all
-CSV.foreach('dump_data/vacation.csv', headers: true, col_sep: ',') do |row|
-  p = Vacation.new(
-    start_at: row["start_at"],
-    duration: row["duration"].to_f,
-    content: row["content"],
-    user_id: (fpo row["user"]),
-    approve: row["approve"],
-    cut: row["cut"])
-  p.save
-  ap p.errors
-end
+# CSV.foreach('dump_data/vacation.csv', headers: true, col_sep: ',') do |row|
+#   p = Vacation.new(
+#     start_at: row["start_at"],
+#     duration: row["duration"].to_f,
+#     content: row["content"],
+#     user_id: (fpo row["user"]),
+#     approve: row["approve"],
+#     cut: row["cut"])
+#   p.save
+#   ap p.errors
+# end
 
 ### run this next to import errand records
-# Errand.delete_all
+Errand.delete_all
 CSV.foreach('dump_data/errand.csv', headers: true, col_sep: ',') do |row|
   p = Errand.new(
     start_at: row["start_at"],
@@ -50,6 +50,7 @@ CSV.foreach('dump_data/errand.csv', headers: true, col_sep: ',') do |row|
     content: row["project"],
     user_id: (fpo row["user"]),
     approve: row["approve"],
+    fee: row["fee"],
     issue: row["issue"],
     created_at: row["created_at"],
     updated_at: row["updated_at"]
@@ -61,17 +62,17 @@ end
 
 ### run this next to import overtime records
 # Overtime.delete_all
-CSV.foreach('dump_data/overtime.csv', headers: true, col_sep: ',') do |row|
-  p = Overtime.new(
-    start_at: row["start_at"],
-    duration: row["duration"].to_f,
-    content: row["content"],
-    user_id: (fpo row["user"]),
-    approve: row["approve"],
-    issue: row["issue"],
-    created_at: row["created_at"],
-    updated_at: row["updated_at"]
-  )
-  p.save
-  ap p.errors
-end
+# CSV.foreach('dump_data/overtime.csv', headers: true, col_sep: ',') do |row|
+#   p = Overtime.new(
+#     start_at: row["start_at"],
+#     duration: row["duration"].to_f,
+#     content: row["content"],
+#     user_id: (fpo row["user"]),
+#     approve: row["approve"],
+#     issue: row["issue"],
+#     created_at: row["created_at"],
+#     updated_at: row["updated_at"]
+#   )
+#   p.save
+#   ap p.errors
+# end

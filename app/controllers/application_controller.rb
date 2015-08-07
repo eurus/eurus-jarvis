8201a228632991ba26e8abc81ffcb949496bed2e
banana <ufozhengli@163.com>
2015-08-07 10:28:13 +0800

    add setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   before_action :authenticate_user!
   layout :layout_by_resource
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+ 
+
   protected
   def layout_by_resource
     if devise_controller? && resource_name == :user
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
-  # skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:check_on_time]
+  
   include Common
   def index
 
@@ -13,7 +14,7 @@ class DashboardController < ApplicationController
     @config = YAML.load_file("config/weather.yml")["code"]
     @response = YahooWeather::Client.new.fetch(12712492)
     if @response
-      code = @response.condition.code 
+      code = @response.condition.code
       @icon = @config[code.to_i]['icon']
     end
 
@@ -36,12 +37,21 @@ class DashboardController < ApplicationController
     end
   end
 
+  def check_on_time
+    @check = {
+      status: 200,
+      info: "签到成功"
+    }
+    respond_to do |format|
+      format.json { render json: @check, status: 200 }
+    end
+  end
   private
 
   def user_params
-    params.require(:user).permit(:username, :nickname, 
-      :password,:avatar,:birthday,
-      :realname,:gender)
+    params.require(:user).permit(:username, :nickname,
+                                 :password,:avatar,:birthday,
+                                 :realname,:gender)
   end
 
 end

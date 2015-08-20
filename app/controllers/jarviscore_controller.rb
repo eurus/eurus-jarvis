0662dd2fe476ed554af8b067f9e7ec7f9d96fa04
banana <ufozhengli@163.com>
2015-08-20 21:29:30 +0800

    test new ruby

--- app/controllers/jarviscore_controller.rb
+++ app/controllers/jarviscore_controller.rb
@@ -1,5 +1,5 @@
 class JarviscoreController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   def wiki
     ap "wiki task"
     ap params

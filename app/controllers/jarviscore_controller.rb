1ae1775e4730885374f12e53fa37cff185ec9e9a
banana <ufozhengli@163.com>
2015-08-20 21:03:26 +0800

    test auto build jekyll site

--- app/controllers/jarviscore_controller.rb
+++ app/controllers/jarviscore_controller.rb
@@ -2,5 +2,11 @@ class JarviscoreController < ApplicationController
   def wiki
     ap "wiki task"
     ap params
+    if Rails.env == 'production'
+      `cd /var/www/eurus-newbee && git pull origin master && jekyll build`
+    else
+      ap 'do nothing'
+    end
+    render nothing: true
   end
 end

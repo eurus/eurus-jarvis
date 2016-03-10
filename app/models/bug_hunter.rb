fa79abc40727fb540c493970641bc19d23907adb
banana <ufozhengli@163.com>
2016-03-10 14:02:17 +0800

    bug hunter

---
+++ app/models/bug_hunter.rb
@@ -0,0 +1,2 @@
+class BugHunter < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201063203) do
+ActiveRecord::Schema.define(version: 20160310060201) do
 
   create_table "articals", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20151201063203) do
     t.string   "origin",     limit: 255
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "bug_hunters", force: :cascade do |t|
+    t.string   "project_name", limit: 255
+    t.text     "trace",        limit: 65535
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "errands", force: :cascade do |t|
